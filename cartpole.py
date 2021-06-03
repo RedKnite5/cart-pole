94781b09ec2208841daf163db592ee19da6007be
RedKnite5 <mr.awesome10000@gmail.com>
2021-06-03 15:07:47 -0700

    initial file

---
+++ cartpole.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+import sys
+import torch
+from torch import nn
+from torch import optim
+
+
+
+print("PyTorch:\t{}".format(torch.__version__))
+
+
+
