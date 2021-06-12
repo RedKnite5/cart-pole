c8e0fe3124327eb8058cc23b1cef3a28650562b0
RedKnite5 <mr.awesome10000@gmail.com>
2021-06-12 13:21:38 -0700

    newer cartpole variation

---
+++ plot.py
@@ -0,0 +1,34 @@
+import sys
+import os
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+import torch
+
+
+def plot_durations():
+    plt.figure()
+    plt.clf()
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration (frames)')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.show()
+
+
+
+dir = sys.argv[1]
+episode_durations = torch.load(os.path.join(dir, "durations.pt"))
+
+plot_durations()
+
