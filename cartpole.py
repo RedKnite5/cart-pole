75628103f7aaf18c3615bb474603800dbf29d7fc
RedKnite5 <mr.awesome10000@gmail.com>
2021-06-15 22:45:27 -0700

    original data

--- cartpole.py
+++ cartpole.py
@@ -1,14 +1,12 @@
+import gym
 import math
 import random
-import os
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from collections import namedtuple, deque
 from itertools import count
-
-import numpy as np
-
 from PIL import Image
-
-import gym
 
 import torch
 import torch.nn as nn
@@ -16,12 +14,6 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as T
 
-import matplotlib
-import matplotlib.pyplot as plt
-
-
-Load = True
-dir = "save_1"
 
 env = gym.make('CartPole-v0').unwrapped
 
@@ -34,7 +26,6 @@ plt.ion()
 
 # if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -54,7 +45,6 @@ class ReplayMemory(object):
 
     def __len__(self):
         return len(self.memory)
-
 
 class DQN(nn.Module):
 
@@ -84,7 +74,6 @@ class DQN(nn.Module):
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
-
 
 resize = T.Compose([T.ToPILImage(),
                     T.Resize(40, interpolation=Image.CUBIC),
@@ -123,11 +112,18 @@ def get_screen():
 
 
 env.reset()
-#plt.figure()
-#plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
-#           interpolation='none')
-#plt.title('Example extracted screen')
-#plt.show()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+           interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
 
 # Get screen size so that we can initialize layers correctly based on shape
 # returned from AI gym. Typical dimensions at this point are close to 3x40x90
@@ -138,99 +134,16 @@ _, _, screen_height, screen_width = init_screen.shape
 # Get number of actions from gym action space
 n_actions = env.action_space.n
 
-
-
-
-
-
-BATCH_SIZE = 16 # default 128
-GAMMA = 0.999
-EPS_START = 1.  # default .9
-EPS_END = 1.0   # default .05
-EPS_DECAY = 3000  # default 200
-TARGET_UPDATE = 50 # default 10
-num_episodes = 100
-
-
-if Load:
-    if not os.path.exists(dir):
-        os.mkdir(dir)
-    
-    try:
-        policy_net_dict = torch.load(os.path.join(dir, "net.pt"))
-        
-        policy_net = DQN(screen_height, screen_width, n_actions)
-        target_net = DQN(screen_height, screen_width, n_actions)
-        
-        policy_net.load_state_dict(policy_net_dict)
-        target_net.load_state_dict(policy_net_dict)
-        target_net.eval()
-    except FileNotFoundError:
-        print("using default net")
-        policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-        target_net = DQN(screen_height, screen_width, n_actions).to(device)
-        
-        target_net.load_state_dict(policy_net.state_dict())
-        target_net.eval()
-    
-    try:
-        memory = torch.load(os.path.join(dir, "memory.pt"))
-    except FileNotFoundError:
-        print("using default memory")
-        memory = ReplayMemory(50_000_000)
-        
-    try:
-        steps_done = torch.load(os.path.join(dir, "steps.pt"))
-    except FileNotFoundError:
-        print("no steps")
-        steps_done = 0
-        
-    try:
-        episode_durations = torch.load(os.path.join(dir, "durations.pt"))
-    except FileNotFoundError:
-        print("no durations")
-        episode_durations = []
-
-else:
-    print("using default net")
-    policy_net = DQN(screen_height, screen_width, n_actions).to(device)
-    target_net = DQN(screen_height, screen_width, n_actions).to(device)
-    
-    target_net.load_state_dict(policy_net.state_dict())
-    target_net.eval()
-    
-    print("using default memory")
-    memory = ReplayMemory(50_000_000)
-    
-    print("no steps")
-    steps_done = 0
-    
-    print("no durations")
-    episode_durations = []
-
-'''
-
-policy_net_dict, memory, steps_done, episode_durations = torch_load("cartpole_data.pth")
-
-policy_net = DQN(screen_height, screen_width, n_actions)
-target_net = DQN(screen_height, screen_width, n_actions)
-
-policy_net.load_state_dict(policy_net_dict)
-target_net.load_state_dict(policy_net_dict)
+policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+target_net = DQN(screen_height, screen_width, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
 target_net.eval()
 
-'''
-
-
-
-
-
-
-
-
-
-
 optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(10000)
+
+
+steps_done = 0
 
 
 def select_action(state):
@@ -239,7 +152,7 @@ def select_action(state):
     eps_threshold = EPS_END + (EPS_START - EPS_END) * \
         math.exp(-1. * steps_done / EPS_DECAY)
     steps_done += 1
-    if sample > eps_threshold:  # I set the hard cutoff
+    if sample > eps_threshold:
         with torch.no_grad():
             # t.max(1) will return largest column value of each row.
             # second column on max result is index of where max element was
@@ -249,13 +162,16 @@ def select_action(state):
         return torch.tensor([[random.randrange(n_actions)]], device=device, dtype=torch.long)
 
 
+episode_durations = []
+
+
 def plot_durations():
     plt.figure(2)
     plt.clf()
     durations_t = torch.tensor(episode_durations, dtype=torch.float)
     plt.title('Training...')
     plt.xlabel('Episode')
-    plt.ylabel('Duration (frames)')
+    plt.ylabel('Duration')
     plt.plot(durations_t.numpy())
     # Take 100 episode averages and plot them too
     if len(durations_t) >= 100:
@@ -267,7 +183,6 @@ def plot_durations():
     if is_ipython:
         display.clear_output(wait=True)
         display.display(plt.gcf())
-
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -314,65 +229,46 @@ def optimize_model():
         param.grad.data.clamp_(-1, 1)
     optimizer.step()
 
+num_episodes = 50
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    env.reset()
+    last_screen = get_screen()
+    current_screen = get_screen()
+    state = current_screen - last_screen
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        _, reward, done, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
 
-try:
-    for i_episode in range(num_episodes):
-        # Initialize the environment and state
-        env.reset()
-        last_screen = get_screen()
+        # Observe new state
+        last_screen = current_screen
         current_screen = get_screen()
-        state = current_screen - last_screen
-        for t in count():
-            # Select and perform an action
-            action = select_action(state)
-            _, reward, done, _ = env.step(action.item())
-            reward = torch.tensor([reward], device=device)
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
 
-            # Observe new state
-            last_screen = current_screen
-            current_screen = get_screen()
-            if not done:
-                next_state = current_screen - last_screen
-            else:
-                next_state = None
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
 
-            # Store the transition in memory
-            memory.push(state, action, next_state, reward)
+        # Move to the next state
+        state = next_state
 
-            # Move to the next state
-            state = next_state
-
-            # Perform one step of the optimization (on the policy network)
-            optimize_model()
-            if done:
-                episode_durations.append(t + 1)
-                plot_durations()
-                break
-        # Update the target network, copying all weights and biases in DQN
-        if i_episode % TARGET_UPDATE == 0:
-            target_net.load_state_dict(policy_net.state_dict())
-            print(i_episode, end="\r")
-            #plot_durations()  # mine
-except (Exception, KeyboardInterrupt) as e:
-    print(f"\nCaught {type(e)}.")
-finally:
-    if Load:
-        print("\nSaving")
-        torch.save(policy_net.state_dict(), os.path.join(dir, "net.pt"))
-        torch.save(memory, os.path.join(dir, "memory.pt"))
-        torch.save(steps_done, os.path.join(dir, "steps.pt"))
-        torch.save(episode_durations, os.path.join(dir, "durations.pt"))
-        
-        
-        #torch.save((policy_net.state_dict(), memory, steps_done, episode_durations), filename)
+        # Perform one step of the optimization (on the policy network)
+        optimize_model()
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+    # Update the target network, copying all weights and biases in DQN
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
 
 print('Complete')
 env.render()
 env.close()
-#plt.ioff()
-#plt.show()
-
-
-
-
+plt.ioff()
+plt.show()
 
